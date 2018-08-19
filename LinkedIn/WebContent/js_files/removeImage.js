896049b63c1c51d24544534362cad7055861e493
deniaAthanasakou <antonia.ath@gmail.com>
2018-08-19 16:03:43 +0300

    Delete removeImage.js

--- LinkedIn/WebContent/js_files/removeImage.js
+++
@@ -1,6 +0,0 @@
-function removeImage() { 
-	document.getElementById("imgInp").value = "";
-	document.getElementById("uploadedImage").src = "";
-	document.getElementById("uploadedImage").style.width = 0;
-	document.getElementById("uploadedImage").style.height = 0;
-}
